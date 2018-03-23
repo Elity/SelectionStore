71c52aed7c24c67497538d001c5ec618f76cc195
Elity <xing.suifeng@gmail.com>
2018-03-23 14:10:22 +0800

    简单封装

---
+++ src/index.js
@@ -0,0 +1,30 @@
+export default function SelectionStore() {
+  let saved = null;
+  return {
+    // 失去焦点时保存选取
+    save() {
+      if (window.getSelection) {
+        let sel = window.getSelection();
+        if (sel.getRangeAt && sel.rangeCount) {
+          return (saved = sel.getRangeAt(0));
+        }
+      } else if (document.selection && document.selection.createRange) {
+        return (saved = document.selection.createRange());
+      }
+      saved = null;
+    },
+
+    // 获取焦点时 恢复选取
+    restore() {
+      if (saved) {
+        if (window.getSelection) {
+          let sel = window.getSelection();
+          sel.removeAllRanges();
+          sel.addRange(saved);
+        } else if (document.selection && saved.select) {
+          saved.select();
+        }
+      }
+    }
+  };
+}
